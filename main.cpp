c39b638be8ee2c32e85db0a1c1b8e2eaf59c656f
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-09-15 20:37:02 +0200

    new

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <raylib.h>
+
+int main() {
+    const int X = 1000;
+    const int Y =  800;
+    const int FPS = 30;
+
+    InitWindow(X, Y, "Gas Particles");
+    SetTargetFPS(FPS);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
