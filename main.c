bd1b8df5663972e26d0b793447e648f1e3853122
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-09-16 22:39:09 +0200

    new

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 #include <raylib.h>
 
 
 #define N 10
 #define radius 10
-
-#define R 8.31
 
 typedef struct {
     Vector2 pos;
@@ -23,28 +22,41 @@ Vector2 VectorSum(Vector2 v, Vector2 w) {
     return (Vector2){v.x + w.x, v.y + w.y};
 }
 
+Vector2 VectorDiff(Vector2 v, Vector2 w) {
+    return (Vector2){v.x - w.x, v.y - w.y};
+}
+
 Vector2 VectorMult(Vector2 v, float scalar) {
     return (Vector2){v.x * scalar, v.y * scalar};
 }
 
+float VectorMagn(Vector2 v) {
+    return (float)sqrt(v.x*v.x + v.y*v.y);
+}
+
 void init_gas(Particle gas[N], int X, int Y, int vmax) {
     for (int n=0; n<N; n++) {
-        gas[n].pos = (Vector2){rand_float(0,1) * (X-R) + R, rand_float(0,1) * (Y-R) + R};
+        gas[n].pos = (Vector2){rand_float(0,1) * (X-radius) + radius, rand_float(0,1) * (Y-radius) + radius};
         gas[n].vel = (Vector2){rand_float(-1,1) * vmax, rand_float(-1,1) * vmax};
         gas[n].acc = (Vector2){rand_float(-1,1), rand_float(-1,1)};
     }
 }
 
 void borders(Particle *particle, int X, int Y) {
-    if (particle->pos.x < R || particle->pos.x > X - R)
+    if (particle->pos.x < radius || particle->pos.x > X - radius)
         particle->vel.x *= -1;
 
-    if (particle->pos.y < R || particle->pos.y > Y - R)
+    if (particle->pos.y < radius || particle->pos.y > Y - radius)
         particle->vel.y *= -1;
 }
 
-void collision(Particle *particle, int X, int Y) {
+void collision(Particle *particle, Particle gas[N]) {
     for (int i=0; i<N; i++) {
+        float dist = VectorMagn(VectorDiff(particle->pos, gas[i].pos));
+        if (dist <= 2*radius) {
+            particle->vel.x *= -1;
+            break;
+        }
     }
 }
 
@@ -70,8 +82,10 @@ int main() {
         ClearBackground(BLACK);
 
         for (int i=0; i<N; i++) {
-            borders(&gas[i], X, Y);
             gas[i].pos = VectorSum(gas[i].pos, VectorMult(gas[i].vel, dt));
+
+            borders(&gas[i], X, Y);
+            collision(&gas[i], gas);
 
             DrawCircleV(gas[i].pos, radius, GRAY);
         }
